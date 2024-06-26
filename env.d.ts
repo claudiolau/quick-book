7b968f7bc1049100867fdc30e08eafca462df760
Claudio Lau <claudio.lau12@gmail.com>
2024-06-25 21:54:13 -0400

    added github oauth

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+	interface ProcessEnv {
+		NODE_ENV: "development" | "production" | "test";
+		PORT?: string;
+
+		// Add other environment variables here
+		NEXT_PUBLIC_AWS_COGNITO_ISSUER: string;
+		NEXT_PUBLIC_AWS_COGNITO_REGION: string;
+		NEXT_PUBLIC_AWS_COGNITO_POOL_ID: string;
+		NEXT_PUBLIC_AWS_COGNITO_APP_CLIENT_ID: string;
+	}
+}
